5f9d62005150403dedf5399fcc7c120fa6c98b1b
king398 <mithilsalunkhe@outlook.com>
2021-11-18 09:17:19 +0530

    a

--- scarp.py
+++ scarp.py
@@ -1,1 +1,6 @@
 import investpy
+
+search_result = investpy.search_quotes(text='reliance', products=['stocks'],
+                                       countries=['india'], n_results=1)
+technical_indicators = search_result.retrieve_technical_indicators(interval='1min')
+print(technical_indicators.head())

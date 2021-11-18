8033225ae1c554128cd9d2a76133c641785fffb5
king398 <mithilsalunkhe@outlook.com>
2021-11-18 10:00:00 +0530

    a

--- scarp.py
+++ scarp.py
@@ -4,11 +4,11 @@ from nsetools import Nse
 
 nse = Nse()
 for i in range(10):
-	search_result = investpy.search_quotes(text='SBIN', products=['stocks'],
+	search_result = investpy.search_quotes(text='BAJFINANCE', products=['stocks'],
 	                                       countries=['india'], n_results=1)
 	technical_indicators = search_result.retrieve_technical_indicators(interval='1min')
 	print(technical_indicators)
 
 	time.sleep(300)
-	q = nse.get_quote("SBIN")
+	q = nse.get_quote("BAJFINANCE")
 	print(q.get("averagePrice"))

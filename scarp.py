732f4685484bec87017dd716f155caf2cee96728
king398 <mithilsalunkhe@outlook.com>
2021-11-18 09:56:48 +0530

    a

--- scarp.py
+++ scarp.py
@@ -1,6 +1,14 @@
 import investpy
+import time
+from nsetools import Nse
 
-search_result = investpy.search_quotes(text='reliance', products=['stocks'],
-                                       countries=['india'], n_results=1)
-technical_indicators = search_result.retrieve_technical_indicators(interval='1min')
-print(technical_indicators.head())
+nse = Nse()
+for i in range(10):
+	search_result = investpy.search_quotes(text='SBIN', products=['stocks'],
+	                                       countries=['india'], n_results=1)
+	technical_indicators = search_result.retrieve_technical_indicators(interval='1min')
+	print(technical_indicators.head())
+
+	time.sleep(300)
+	q = nse.get_quote("SBIN")
+	print(q.get("averagePrice"))

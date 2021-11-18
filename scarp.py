89e2d027afe578e8452696fb4b40846c3626b1a1
king398 <mithilsalunkhe@outlook.com>
2021-11-18 10:16:54 +0530

    a

--- scarp.py
+++ scarp.py
@@ -1,14 +1,28 @@
 import investpy
 import time
 from nsetools import Nse
+import numpy as np
+from datetime import datetime
 
+CODE = ["BAJFINANCE", "SBIN"]
 nse = Nse()
-for i in range(10):
-	search_result = investpy.search_quotes(text='BAJFINANCE', products=['stocks'],
-	                                       countries=['india'], n_results=1)
-	technical_indicators = search_result.retrieve_technical_indicators(interval='1min')
-	print(technical_indicators)
+end_dict = {}
+for n in range(10):
+	technical = []
+	prices = []
+	for i in CODE:
+		search_result = investpy.search_quotes(text=i, products=['stocks'],
+		                                       countries=['india'], n_results=1)
+		technical_indicators = search_result.retrieve_technical_indicators(interval='1min')
+		technical_indicators["value"] = technical_indicators["value"].astype(np.float)
+		technical.append(technical_indicators["value"].values)
 
-	time.sleep(300)
-	q = nse.get_quote("BAJFINANCE")
-	print(q.get("averagePrice"))
+	time.sleep(10)
+	for p in CODE:
+		q = nse.get_quote(p)
+		prices.append(float(q.get("averagePrice")))
+	for a, b in zip(technical, prices):
+		now = datetime.now()
+
+		current_time = now.strftime("%H:%M:%S")
+		print(np.stack(a, b))

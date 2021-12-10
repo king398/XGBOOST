8bc7ca0d5c3136a327a1ae048d5e373891d192ba
king398 <mithilsalunkhe@outlook.com>
2021-12-10 20:57:22 +0530

    a

--- LSTM.py
+++ LSTM.py
@@ -1,11 +1,15 @@
 import pandas as pd
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
 df = pd.read_csv("F:\Pycharm_projects\XGBOOST\data\Reliance_data.csv")
 del df['timestamp']
 target_sensor = "Price"
 features = list(df.columns.difference([target_sensor]))
 
-forecast_lead = 15
+forecast_lead = 2
 target = f"{target_sensor}_lead{forecast_lead}"
 
 df[target] = df[target_sensor].shift(-forecast_lead)
 df = df.iloc[:-forecast_lead]
+display(df)
